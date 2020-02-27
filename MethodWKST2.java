97424ce53b77ed6cf90c898186c1d378b0261e80
jas40k <60977683+jas40k@users.noreply.github.com>
2020-02-27 07:43:12 -0500

    Add files via upload

---
+++ MethodWKST2.java
@@ -0,0 +1,22 @@
+
+public class MethodWKST2
+{
+   public static void main(String[] args)
+   {
+   barrier();
+   barrier();
+   address();
+   barrier();
+   barrier();
+   barrier();
+   }
+   public static void barrier()
+   {
+      System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+   }
+   public static void address()
+   {
+      System.out.println("Jake Smith\n29 Hadler Drive\nSomerset NJ 08873");
+   }
+   
+}
