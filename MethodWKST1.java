affca7de2803d8c0f1db321357355ebdaf5285df
jas40k <60977683+jas40k@users.noreply.github.com>
2020-02-27 07:42:37 -0500

    Add files via upload

---
+++ MethodWKST1.java
@@ -0,0 +1,19 @@
+public class MethodWKST1
+{
+   public static void main(String[] args)
+   {
+      Stanza1();
+      Stanza1();
+      Stanza2();
+      Stanza2();
+      System.out.println("They all run after the farmer's wife.");
+   }
+   public static void Stanza1()
+   {
+      System.out.println("Three blind mice");
+   }
+   public static void Stanza2() 
+   {
+      System.out.println("See how they run");
+   }
+}
